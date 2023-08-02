061542255d52c82b5ca6384876082d3610f0d101
Balram Uddhav Melkunde <102988461+BalramMEL@users.noreply.github.com>
2023-08-02 23:59:27 +0530

    Navbar and footer added

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Container from '../Container'
+import FooterList from './FooterList'
+import Link from 'next/link'
+import {MdFacebook} from 'react-icons/md'
+import {AiFillTwitterCircle, AiFillInstagram, AiFillYoutube} from 'react-icons/ai'
+
+const Footer = () => {
+  return (
+    <footer className='bg-slate-700 text-slate-200 text-sm mt-16'>
+          <Container>
+              <div className='flex flex-col md:flex-row justify-between pt-16 pb-8'>
+                  <FooterList>
+                      <h3 className='text-base font-bold mb-2'>Shop Categories</h3>
+                      <Link href='#'>Phones</Link>
+                      <Link href='#'>Laptops</Link>
+                      <Link href='#'>Desktops</Link>
+                      <Link href='#'>Watches</Link>
+                      <Link href='#'>TVs</Link>
+                      <Link href='#'>Accessories</Link>
+                  </FooterList>
+
+                  <FooterList>
+                      <h3 className='text-base font-bold mb-2'>Customer Service</h3>
+                      <Link href='#'>Contact Us</Link>
+                      <Link href='#'>Shipping Policy</Link>
+                      <Link href='#'>Returns & Exchanges</Link>
+                      <Link href='#'>Watches</Link>
+                      <Link href='#'>FAQs</Link>                      
+                  </FooterList>
+
+                  <div className='w-full md:w-1/3 mb-6 md:mb-0'>
+                    <h3 className='text-base font-bold mb-2'>About Us</h3>
+                    <p>At our electonics store, we are dedicated to providing the latest and greates devices and accessories to our customers. With a wide selection of phones, TVs, Laptops, watches and accessories.</p>
+                    <p className='mt-2'>© {new Date().getFullYear()} E~shop. All rights reserved </p>                
+                  </div>
+
+                  <FooterList>
+                      <h3 className='text-base font-bold mb-2'>Follow us</h3>
+                      <div className='flex gap-2'>
+                        <Link href='#'><MdFacebook size={24} /></Link>                      
+                        <Link href='#'><AiFillTwitterCircle size={24} /></Link>                      
+                        <Link href='#'><AiFillInstagram size={24} /></Link>                      
+                        <Link href='#'><AiFillYoutube size={25} /></Link>   
+                      </div>
+                  </FooterList>
+              </div>    
+          </Container> 
+
+    </footer>
+  )
+}
+
+export default Footer
---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Container from '../Container'
+import Link from 'next/link'
+import { Redressed } from 'next/font/google'
+
+const redressed = Redressed({ subsets: ["latin"], weight: ["400"]});
+
+const Navbar = () => {
+  return (
+    <div
+        className='
+            sticky
+            top-0
+            z-30
+            bg-slate-200
+            w-full
+            shadow-sm
+          '
+      >
+          <div className='py-4 border-b-[1px]'>
+              <Container>
+                  <div
+                      className='
+                          flex
+                          items-center
+                          justify-between
+                          gap-3
+                          md:gap-0    
+                  '>
+                      <Link href='/' className={`${redressed.className} font-bold text-2xl`}>
+                          E-shop
+                      </Link> 
+                      <div className='hidden md:block'>
+                          Search
+                      </div>
+                      <div className='flex gap-8 md:gap-12 items-center'>
+                          <div>CartCount</div>
+                          <div>UserMenu</div>
+                      </div>
+                  </div>
+              </Container>
+          </div>
+          
+    </div>
+  )
+}
+
+export default Navbar
